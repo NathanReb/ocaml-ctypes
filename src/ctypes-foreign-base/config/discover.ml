f58836c5d07acc4b1902f3478d2c8178a764877c
Anil Madhavapeddy <anil@recoil.org>
2019-05-18 09:53:26 -0400

    move as-needed test to ctypes-foreign-base

--- src/ctypes-foreign-base/config/discover.ml
+++ src/ctypes-foreign-base/config/discover.ml
@@ -18,8 +18,15 @@ let () =
         match C.ocaml_config_var_exn c "system" with
         | "Win32" -> "win"
         | _ -> "unix" in
+
+      let f = "as_needed_test" in
+      let ml = f ^ ".ml" in
+      open_out ml |> close_out;
+      let res = C.Process.run_ok c "ocamlopt" ["-shared"; "-cclib"; "-Wl,--no-as-needed"; ml; "-o"; f^".cmxs"] in
+      let extra_ldflags = if res then ["-Wl,--no-as-needed"] else [] in
+
       C.Flags.write_sexp "c_flags.sexp" conf.cflags;
       C.Flags.write_lines "c_flags" conf.cflags;
-      C.Flags.write_sexp "c_library_flags.sexp" conf.libs;
+      C.Flags.write_sexp "c_library_flags.sexp" (conf.libs @ extra_ldflags);
       C.Flags.write_lines "backend.sexp" [backend]
     )
